17742e2d8755f92f025486487d9181ec0800fd8c
Guillermo Fitz Patrick <willie@mbpWillie.home>
2018-08-30 16:38:39 -0300

    server added

---
+++ server/Hubs/cacheHub.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Server.Hubs
+{
+    public class cacheHub : Hub
+    {
+      //  public async Task SendMessage(string user, string message)
+      //   {
+      //       await Clients.All.SendAsync("ReceiveMessage", user, message);
+      //   }
+
+        public Task Send(string data)
+        {
+            return Clients.All.SendAsync("Send", data);
+        }
+
+    }
+}
